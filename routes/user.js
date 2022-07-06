efbbdb8406ab69b66d36ac60d1b64c220b592a8e
Marianosandez <marianosandez@gmail.com>
2022-07-06 10:41:36 -0300

    register/login connected

--- routes/user.js
+++ routes/user.js
@@ -4,41 +4,18 @@ const passport = require("passport");
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res) => {
-  const response = await User.findOne({ email: req.body.email });
-  if (response) res.status(400).json({ message: "user already exist" });
+  const response = await User.findOne({ name: req.body.name });
+  if (response) res.status(409).json({ message: "user already exist" });
   else {
     const newUser = await User.create(req.body);
-    console.log(newUser);
     res.status(201).send(newUser);
   }
 });
 
-userRouter.post("/login", passport.authenticate("local"), (req, res) => {
+userRouter.post("/login", passport.authenticate("local", {}), (req, res) => {
   const id = req.user._id.toString();
-  const email = req.user.email;
-  res.json({ id: id, email: email });
+  const name = req.user.name;
+  res.json({ id: id, name: name });
 });
-// router.post("/register", (req, res) => {
-//   User.create(req.body).then((user) => {
-//     res.status(201).send(user);
-//   });
-// });
-
-// newsRouter.get("/newses", (req, res, next) => {
-//   News.find({})
-//     .then((data) => {
-//       res.send(data);
-//     })
-//     .catch(next);
-// });
-
-// newsRouter.get("/:id", (req, res, next) => {
-//   const { id } = req.params;
-//   News.findById(id)
-//     .then((data) => {
-//       res.send(data);
-//     })
-//     .catch(next);
-// });
 
 module.exports = userRouter;
