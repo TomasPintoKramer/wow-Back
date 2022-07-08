0447c2c86369418c8e2dfafa1411b2c798963f59
Tomas Pinto Kramer <tomaspintokramer@MacBook-Air-de-Tomas.local>
2022-07-08 12:33:39 -0300

    fixed for heroku6

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const News = require("../models/News");
 const express = require("express");
 const Router = express.Router();
 const routerNews = require("./news");
-const routerUser = require("./User");
+const routerUser = require("./user");
 
 Router.use("/news", routerNews);
 Router.use("/user", routerUser);
