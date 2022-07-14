6e2f0ff78e03d376bf200f5c095a41d485d07d06
Tomas Pinto Kramer <tomaspintokramer@MacBook-Air-de-Tomas.local>
2022-07-14 20:53:37 -0300

    fixed

--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://userWow:wow2022@wowdb.bjojf.mongodb.net/wowDb?retryWrites=true&w=majority";
 
-//  mongoose
-//   .connect(connectionString)
-//   .then(() => console.log("Data Base connected"))
-//   .catch((err) => console.log(err));
+ mongoose
+  .connect(connectionString)
+  .then(() => console.log("Data Base connected"))
+  .catch((err) => console.log(err));
 
 //Para seedear modificar db.js con esta funcion
-module.exports = async () => {
-  await mongoose.connect(connectionString);
-  console.log("Data Base connected");
-  return mongoose;
-};
+// module.exports = async () => {
+//   await mongoose.connect(connectionString);
+//   console.log("Data Base connected");
+//   return mongoose;
+// };
